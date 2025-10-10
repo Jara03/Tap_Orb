1a313be249386f2c505b857a4ed99ad34dd81f4f
Rania <raniabensaid.rbs@gmail.com>
2025-10-11 01:18:10 +0200

    added basic starting UI

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -1,23 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
+    public GameObject levelSelector;
+    public GameObject worldSelector;
+    public GameObject startButton;
+
+    private int worldSelected = 0;
+    
     // Start is called before the first frame update
     void Start()
     {
+        worldSelector.SetActive(false);
+        levelSelector.SetActive(false);
         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
         
     }
     
-    void LoadLevel(int level)
+    public void toggleLevelSelection(int worldNumber)
     {
-        Application.LoadLevel(level);
+        worldSelected = worldNumber;
+        worldSelector.SetActive(false);
+        levelSelector.SetActive(true);
+    }
+
+    
+   public void LoadLevel(int level)
+    {
+        //charger une scene : le monde séléctionné et le niveau choisi 
+        
+        SceneManager.LoadScene("Monde"+worldSelected);
+        
+        //puis indiquer les bonnes infos a la scene chargée
+    }
+   
+   public void StartGameButtonClicked()
+    {
+        //activer le world selector
+        worldSelector.SetActive(true);
+        //levelSelector.SetActive(false);
+        startButton.SetActive(false);
+        
     }
 }
